7190ffa3613271cabb6a4029d5dbc8a4721913b6
desiassassin <arunksehrawat@gmail.com>
2023-04-14 17:07:20 +0530

    Added custom controls

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,15 @@
-import { useState } from "react";
-import { ReactComponent as PlayButton } from "./assets/playButton.svg";
+import { useEffect, useState } from "react";
+import { ReactComponent as PlayButton } from "./assets/svg/playButton.svg";
+import { ReactComponent as RestartButton } from "./assets/svg/restartButton.svg";
+import { ReactComponent as MutedButton } from "./assets/svg/mutedButton.svg";
+import { ReactComponent as SoundButton } from "./assets/svg/soundButton.svg";
+import { ReactComponent as FullscreenButton } from "./assets/svg/fullscreenButton.svg";
 
 // import short videos
-import caterpillarVideo from "./assets/caterpillar.mp4";
-import fishVideo from "./assets/fish.mp4";
-import noiseVideo from "./assets/noise.mp4";
-import rabbitVideo from "./assets/rabbit.mp4";
+import caterpillarVideo from "./assets/videos/caterpillar.mp4";
+import fishVideo from "./assets/videos/fish.mp4";
+import noiseVideo from "./assets/videos/noise.mp4";
+import rabbitVideo from "./assets/videos/rabbit.mp4";
 
 function App() {
      const [videos, setVideos] = useState([
@@ -30,9 +34,24 @@ function App() {
                show: false
           }
      ]);
-     const currentVideo = videos.filter(({ show }) => show);
+     const [currentVideo] = videos.filter(({ show }) => show);
      const [videoIsPlaying, setVideoIsPlaying] = useState(false);
+     const [videoIsMuted, setVideoIsMuted] = useState(false);
      const [end, setEnd] = useState(false);
+
+     useEffect(() => {
+          const updateTrackBarInterval = setInterval(() => {
+               const video = document.getElementById(`video-${currentVideo.name}`);
+               const trackbar = document.getElementById("track-bar");
+               const { currentTime, duration, paused } = video;
+
+               if (!paused) trackbar.value = (currentTime / duration) * 100;
+          }, 100);
+
+          return () => {
+               clearInterval(updateTrackBarInterval);
+          };
+     }, []);
 
      function playPauseCurrentVideo(event) {
           // stop the event from going to the parent
@@ -83,18 +102,32 @@ function App() {
      return (
           <>
                {/* Render Videos */}
-               {currentVideo.map(({ name, src, show }) => (
-                    <video key={name} id={`video-${name}`} className={!show ? "hidden" : ""} src={src} muted></video>
-               ))}
-
+               {/* {currentVideo.map(({ name, src, show }) => ( */}
+               <video
+                    // key={currentVideo.name}
+                    id={`video-${currentVideo.name}`}
+                    className={!currentVideo.show ? "hidden" : ""}
+                    src={currentVideo.src}
+                    muted
+               ></video>
+               {/* ))} */}
                {/* custom controls */}
                <div id="control-wrapper" onClick={playPauseCurrentVideo}>
-                    <div id="control-bar"></div>
+                    <div id="control-bar" onClick={(event) => event.stopPropagation()}>
+                         <RestartButton id="restart-button" />
+                         <input id="track-bar" type="range" min={0} max={100} step={1} />
+                         {videoIsMuted ? (
+                              <MutedButton id="muted-button" onClick={() => setVideoIsMuted(false)} />
+                         ) : (
+                              <SoundButton id="sound-button" onClick={() => setVideoIsMuted(true)} />
+                         )}
+                         <FullscreenButton id="fullscreen-button" />
+                    </div>
                     {/* <button id="previous-button" onClick={playPreviousVideo}>
                          Previous Video
                     </button> */}
                     {!videoIsPlaying && (
-                         <div id="main-play-button" onClick={playPauseCurrentVideo}>
+                         <div id="play-button" onClick={playPauseCurrentVideo}>
                               <PlayButton />
                          </div>
                     )}
@@ -102,7 +135,6 @@ function App() {
                          Next Video
                     </button> */}
                </div>
-
                {/* End screen */}
                <div id="end-screen" className={end ? "show" : ""}>
                     Interactive video has ended. Reload the page.
