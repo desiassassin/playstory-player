003054cf225536515af97731136a7c9066a956b7
desiassassin <arunksehrawat@gmail.com>
2023-04-24 16:51:25 +0530

    updated backend url

--- client/src/env.js
+++ client/src/env.js
@@ -1,4 +1,4 @@
-const BASE_URL = import.meta.env.VITE_APP_BASE_URL || "http://run.playstory.io";
+const BASE_URL = import.meta.env.VITE_APP_BASE_URL || "https://run.playstory.io";
 const VIDEO_SERVER_URL = import.meta.env.VITE_APP_VIDEO_SERVER_URL || "https://videos.playstory.io";
 
 export { BASE_URL, VIDEO_SERVER_URL };
